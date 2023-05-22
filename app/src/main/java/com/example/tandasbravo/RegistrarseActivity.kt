c2a0a4fa82b785c78ede34bc2d043154e6d45cae
Mariohdz20 <mario.hernandez02@iest.edu.mx>
2023-05-22 17:22:18 -0600

    Segundo commit

--- app/src/main/java/com/example/tandasbravo/RegistrarseActivity.kt
+++ app/src/main/java/com/example/tandasbravo/RegistrarseActivity.kt
@@ -1,11 +1,29 @@
 package com.example.tandasbravo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class RegistrarseActivity : AppCompatActivity() {
+
+    private var bnRegistrarse: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
+
+        inicializarVistas()
+        bnRegistrarse?.setOnClickListener {
+            val i = Intent( this@RegistrarseActivity, MainActivity::class.java)
+            startActivity(i)
+            Toast.makeText(this, "Su registro de usuario ha sido exitoso", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+    fun inicializarVistas() {
+        bnRegistrarse = findViewById(R.id.registrarse)
+    }
+
 }
